204b119caa9bd7bfc1c38a6f8682502f5910e734
ZhangXiaoyu-Chief <61411916@qq.com>
2016-01-15 18:03:39 +0800

    homework shoping 20160115 白天2

---
+++ mylib/output.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+
+def myljust(str1, width, fillchar = None):
+    '''
+    中英文混合左对齐
+    :param str1: 欲对齐字符串
+    :param width: 宽度
+    :param fillchar: 填充字符串
+    :return: 新的经过左对齐处理的字符串对象
+    '''
+    if fillchar == None:
+        fillchar = ' '
+    length = len(str1.encode('gb2312'))
+    fill_char_size = width - length if width >= length else 0
+    return "%s%s" %(str1, fillchar * fill_char_size)
+
+
+def myrjust(str1, width, fillchar = None):
+    '''
+    中英文混合右对齐
+    :param str1: 欲对齐字符串
+    :param width: 宽度
+    :param fillchar: 填充字符串
+    :return: 新的经过右对齐处理的字符串对象
+    '''
+    if fillchar == None:
+        fillchar = ' '
+    length = len(str1.encode('gb2312'))
+    fill_char_size = width - length if width >= length else 0
+    return "%s%s" %(fillchar * fill_char_size, str1)
+
+def mycenter(str1, width, fillchar = None):
+    '''
+    中英文混合居中对齐
+    :param str1: 欲对齐字符串
+    :param width: 宽度
+    :param fillchar: 填充字符串
+    :return: 新的经过居中对齐处理的字符串对象
+    '''
+    if fillchar == None:
+        fillchar = ' '
+    length = len(str1.encode('gb2312'))
+    fill_char_size = width - length if width >= length else 0
+    if length%2 == 0:
+        return "%s%s%s" %(fillchar * (fill_char_size //2), str1, fillchar* (fill_char_size // 2))
+    else:
+        return "%s%s%s" %(fillchar * (fill_char_size //2 + 1), str1, fillchar* (fill_char_size // 2))
+
+def color(str1, color_id = 37):
+    '''
+    字符串颜色高亮
+    :param str1: 源字符串
+    :param color_id: 颜色id：30:黑 31:红 32:绿 33:黄 34:蓝色 35:紫色 36:深绿 37:白色
+    :return: 返回高亮字符串
+    '''
+    return "\33[%sm%s\033[0m" %(color_id, str1)
+
+def myfind(str1, sub, color_id = None):
+    if color_id == None:
+        color_id = 31
+    return str1.replace(sub, color(sub, color_id))
+
+
+print(myljust('中文',20,'*'))
+print(myljust('abc',20,'*'))
+print(myljust('中文abc',20,'*'))
+print(myrjust('中文',20,'*'))
+print(myrjust('abc',20,'*'))
+print(myrjust('中文abc',20,'*'))
+print(mycenter('中文',20,'*'))
+print(mycenter('abc',20,'*'))
+print(mycenter('中文abc',20,'*'))
+
+print(color('红色', 31))
+print(color('黄色', 33))
+print(color('绿色', 32))
+print(color('蓝色', 34))
+
+print(myfind('Pythonathon', 'thon'))
+
+
