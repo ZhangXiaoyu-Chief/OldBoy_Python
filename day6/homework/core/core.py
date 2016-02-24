647b5e361c6371f2ff231fe721a0a9a932a1db76
张晓宇 <61411916@qq.com>
2016-02-24 16:45:40 +0800

    20160224 白天

--- day6/homework/core/core.py
+++ day6/homework/core/core.py
@@ -3,13 +3,17 @@
 from conf import conf
 from model import role
 
-def print_main(leading_role):
-    pass
-    role_info = leading_role.get_info()
+
+me = None
+wanqing = None
+
+def play_main():
+    global me
+    role_info = me.get_info()
 
     main_map = '''
 返回主菜单(r)  存档(s)   查看背包(p)
-%s: 生命 %s   声望 %s   现银 %s   银票 %s   欠账 %s   level %s %s %s   穿越天数 %s
+%s: 生命 %s   声望 %s   现银 %s   银票 %s   欠账 %s   包裹 %s   穿越天数 %s
 ---------------------------------------------------------------------------------
                               北市(2)
                         |                    |
@@ -24,10 +28,11 @@ def print_main(leading_role):
            医馆(6)      |                    |     丽春院(8)
                         |                    |
                              南市(7)
-    ''' %(role_info[0], role_info[1], role_info[2], role_info[3], role_info[4], role_info[5], role_info[6], role_info[7], role_info[8], role_info[9])
+    ''' %(role_info[0], role_info[1], role_info[2], role_info[3], role_info[4], role_info[5], role_info[6], role_info[7])
     print(main_map)
 
 def new_game():
+    global me
     me = role.leading_role(conf.LEADING_ROLE_INIT_DATA)
     name = input('请输入玩家的姓名: ').strip()
     if name:
@@ -48,30 +53,28 @@ def new_game():
     # input('于是我出了姑娘的屋子，来到街上')
     flag = True
     while flag:
-        print_main(me)
+        play_main()
         chose = input('>> ').strip()
-        main_menu_do = {"2":market}
+        main_menu_do = {"1" : bank, "2" : market, "4" : market, "5" : market, "6" : hospital, "7" : market}
         if chose in main_menu_do.keys():
-            main_menu_do[chose](me)
+            main_menu_do[chose](chose)
         if chose == 'r':
             flag = False
 
-def buy_goods(me, seller, goods, price):
+def buy_goods(seller, goods, price):
+    global me
     import re
-
-
     while True:
         count = seller.say('你要多少(返回r)').strip()
-
         if count == 'r':
             return False
         me.say(count)
         if re.match('^\d+$', count):
-            max_count = me.get_goods_count()
+            free_count = me.get_free_count()
             cash = me.get_cash()
             total = price * int(count)
             if int(total) <= cash:
-                if int(count) <= max_count:
+                if int(count) <= free_count:
                     seller.say('好嘞，客官，这是您的%s个%s' %(count, goods['name']))
                     me.buy_goods(goods['name'], int(count), total)
                     return True
@@ -81,36 +84,129 @@ def buy_goods(me, seller, goods, price):
             else:
                 seller.say('客官，您好像没那么多银子')
                 me.say('对哦，我再考虑一下')
-
-
-
-
-
         else:
             seller.say('客官，我听不懂你说什么，Can you speak chinese？')
-def sale_goods():
+def sale_goods( seller, goods, price):
+    global me
+    import re
+    while True:
+        count = seller.say('你要卖多少(返回r)').strip()
+        if count == 'r':
+            return False
+        me.say(count)
+        if re.match('^\d+$', count):
+            max_count = me.find_goods_count(goods['name'])
+            #cash = me.get_cash()
+            total = price * int(count)
+
+            if int(count) <= max_count:
+                seller.say('好嘞，客官，这是您的%s两银子' %total)
+                #me.buy_goods(goods['name'], int(count), total)
+                me.sale_goods(goods['name'], int(count), total)
+                return True
+            else:
+                seller.say('客官，您的背包好像没有那么多%s吧' %goods['name'])
+                me.say('对哦，我再考虑一下')
+        else:
+            seller.say('客官，我听不懂你说什么，Can you speak chinese？')
+
+
+def hospital( *args):
+    global me
+    doctor = role.role('江湖郎中')
+    doctor.say('老夫人称华佗在世，当年可是御医，专门给后宫的娘娘看妇科的')
+    me.think('妇科？？什么鬼？')
+    doctor.say('这位客官，有何贵干')
+    me.think('废话，找你当然是看病了')
+    me.say('大夫，我不太舒服')
+    doctor.say('来来来，我给你号号脉')
+    input('过了一盏茶的功夫')
+    if me.get_hp() == 100:
+        doctor.say('这位客官，我看你脉象平稳，不像得了什么病，您是来消遣老夫的吗？')
+    else:
+        doctor.say('这位客官，你没什么大碍，只是有些肾亏，我给你开几服药调养调养就好了')
+        doctor.say('一共需要100两')
+        if me.get_cash() > 100:
+            me.pay(100)
+            me.add_hp(5)
+            me.say('啥？这么贵，真是黑心')
+            input('不情愿的交了银子走了')
+        else:
+            me.say('我没那么多银子啊')
+            doctor.say('没银子还来看病，滚')
+            me.say('此处不留爷，自有留爷处')
+
+
+def deposit(zhanghui):
+    global me
+    import re
+    while True:
+        money = zhanggui.say('您要开多少的银票>> ')
+        me.say(money)
+        if re.match('^\d+$', money):
+            if me.get_cash() >= money:
+                zhanggui.say('客官，这是您%s两的银票，您收好了，欢迎您再来')
+                return True
+            else:
+                zhanggui.say('客官，我听不懂你说什么，Can you speak chinese？我问您要开多少的银票>> ')
+        else:
+            zhanggui.say('客官，我听不懂你说什么，Can you speak chinese？我问您要开多少的银票>> ')
+def take_cash(zhanggui):
     pass
 
+def bank(*args):
+    global me
+    import re
+    zhanggui = role.role('钱庄掌柜')
+    chose = zhanggui.say('客官，您是开银票(1)还是兑换银票(2)(退出0)>> ')
+    chose_do = {"1" : deposit, "2" : take_cash}
+    flag = True
+    flag2 = True
+    while flag:
+        if chose in chose_do.keys():
+            if chose == '1':
+                me.say('开银票')
+                if deposit(zhanggui):
 
-def market(me):
+                    break
+            else:
+                me.say('兑换银票')
+            if chose_do[chose](zhanggui):
+                break
+
+        elif chose == '0':
+            break
+        else:
+            chose = zhanggui.say('客官，我听不懂你说什么，Can you speak chinese？您是开银票(1)还是兑换银票(2)(退出0)>> ')
+
+
+def market( market_id):
+    global me
     import random
+    is_do = False
     goods_list = conf.GOODS_list
     # for( i = 0; i < GOODS_LIST_MAX; i++ ){
     #
 	# 	plist[i] = goods_list[rlist[i]].min_price + rand32() % ( abs( goods_list[rlist[i]].max_price - goods_list[rlist[i]].min_price ) + 1 );
 	# }
-    prices = []
-    for num, goods in enumerate(goods_list, 1):
-        price = goods['min'] + random.random() * (goods['max'] - goods['min'] + 1)
+    # prices = []
+    # for num, goods in enumerate(goods_list, 1):
+    #     price = goods['min'] + random.random() * (goods['max'] - goods['min'] + 1)
+    #
+    #     prices.append(int(price))
+    names = {"2" : "北市商贩", "4" : "西市商贩", "5" : "东市商贩", "7" : "南市商贩"}
+    seller = role.seller(names[market_id])
 
-        prices.append(int(price))
-    seller = role.role('商贩')
-    seller.say(random_news(prices))
+    # seller.say(random_news(prices))
+    seller.say_news()
+    prices = seller.get_prices()
+
     seller.say('客官，您需要点什么，我这里应有皆有，价格公道')
     me.say('我看看')
 
     # print(goods_list)
     # print(enumerate(goods_list, 1))
+
 
 
 
@@ -129,14 +225,13 @@ def market(me):
             me.say('%s' %chose_goods['name'])
             chose_do = seller.say('%s？您是买(1)还是卖(2)还是不要(0)' %chose_goods['name']).strip()
             while True:
-
                 if chose_do in chose_do_menu.keys():
-                    if(chose_do_menu[chose_do](me, seller, chose_goods, prices[int(chose) - 1])):
+                    if(chose_do_menu[chose_do](seller, chose_goods, prices[int(chose) - 1])):
                         seller.say('客官您还看点啥')
+                        is_do = True
                         break
                     else:
                         me.say('不不，还是不要%s了'  %chose_goods['name'])
-
                         seller.say('那您要啥？')
                         break
                 elif chose_do == '0':
@@ -146,9 +241,12 @@ def market(me):
                 else:
                     me.say(chose_do)
                     chose_do = seller.say('客官，我听不懂你说什么，Can you speak chinese？您是买(1)还是卖(2)还是不要(0)')
-
         elif chose == '0':
-            seller.say("客官，欢迎您下次再来")
+            if is_do:
+                seller.say("客官，欢迎您下次再来")
+            else:
+                seller.say('啥都不干进来干吗，去去去')
+                me.say('此处不留爷，自有留爷处')
             me.go_one_day()
             print(me.goods_list)
             break
@@ -159,42 +257,42 @@ def market(me):
 
 
 
-def random_news(prices):
-    import random
-    news_list = conf.NEWS_LIST
-    # int rd = rand32() % GOODS_LIST_MAX;
-    #
-	# system( "cls" );
-    #
-	# puts( "-北京新闻播报-\n" );
-    #
-	# //随机选择新闻
-	# int news_id = rlist[rd] * 4 + rand32() % 4;
-    rd = random.randrange(0, len(prices))
-    news_id = rd * 4 + random.randrange(0, 4)
-    # print(len(news_list))
-    #news = news_list[random.randrange(0, 35585 ) % len(news_list)]
-    news = news_list[news_id]
-    # if( news_list[news_id].impact > 0 ){
-    #
-	# 	plist[rd] = ( int )( plist[rd] * news_list[news_id].impact );
-	# }
-	# else if( news_list[news_id].impact < 0 ){
-    #
-	# 	plist[rd] = ( int )( plist[rd] / ( -news_list[news_id].impact ) );
-	# }
-
-    if news['impact'] > 0:
-        # print(prices[news['id']])
-        prices[news['id']] = int(prices[news['id']] * news['impact'])
-        # print(prices[news['id']])
-    else:
-        # print(prices[news['id']])
-        prices[news['id']] = int(prices[news['id']] / (-news['impact']))
-        # print(prices[news['id']])
-    #print('---世界消息---')
-    # input(news['msg'])
-    return news['msg']
+# def random_news(prices):
+#     import random
+#     news_list = conf.NEWS_LIST
+#     # int rd = rand32() % GOODS_LIST_MAX;
+#     #
+# 	# system( "cls" );
+#     #
+# 	# puts( "-北京新闻播报-\n" );
+#     #
+# 	# //随机选择新闻
+# 	# int news_id = rlist[rd] * 4 + rand32() % 4;
+#     rd = random.randrange(0, len(prices))
+#     news_id = rd * 4 + random.randrange(0, 4)
+#     # print(len(news_list))
+#     #news = news_list[random.randrange(0, 35585 ) % len(news_list)]
+#     news = news_list[news_id]
+#     # if( news_list[news_id].impact > 0 ){
+#     #
+# 	# 	plist[rd] = ( int )( plist[rd] * news_list[news_id].impact );
+# 	# }
+# 	# else if( news_list[news_id].impact < 0 ){
+#     #
+# 	# 	plist[rd] = ( int )( plist[rd] / ( -news_list[news_id].impact ) );
+# 	# }
+#
+#     if news['impact'] > 0:
+#         # print(prices[news['id']])
+#         prices[news['id']] = int(prices[news['id']] * news['impact'])
+#         # print(prices[news['id']])
+#     else:
+#         # print(prices[news['id']])
+#         prices[news['id']] = int(prices[news['id']] / (-news['impact']))
+#         # print(prices[news['id']])
+#     #print('---世界消息---')
+#     # input(news['msg'])
+#     return news['msg']
 
 
 
--- day6/homework/model/role.py
+++ day6/homework/model/role.py
@@ -35,19 +35,26 @@ class leading_role(role):
         self.ndays = leading_role_info['ndays']
         self.goods_list = leading_role_info['goods_list']
         self.max_goods_list = leading_role_info['max_goods_list']
-        self.goods_count = 0
+        self.goods_total_count = 0
+        self.one_day_cash = 0
 
     def get_name(self):
         return self.name
     def get_info(self):
         # print('ss')
-        role_info = (self.name, self.hp, self.rp, self.cash, self.deposit, self.debt, self.level[0], self.level[1], self.level[2], self.ndays)
+        role_info = (self.name, self.hp, self.rp, self.cash, self.deposit, self.debt, self.goods_total_count, self.ndays)
         return role_info
     def think(self, msg):
         input(mylib.color(msg, 32))
 
-    def get_goods_count(self):
-        return self.max_goods_list - self.goods_count
+    def get_free_count(self):
+        return self.max_goods_list - self.goods_total_count
+
+    def find_goods_count(self, goods_name):
+        for item in self.goods_list:
+            if item['name'] == goods_name:
+                return item['count']
+        pass
     def get_cash(self):
         return self.cash
 
@@ -62,13 +69,104 @@ class leading_role(role):
             tmp_goods['name'] = goods_name
             tmp_goods['count'] = count
             self.goods_list.append(tmp_goods)
-        self.goods_count += count
+        self.goods_total_count += count
+        self.one_day_cash -= price
         self.cash -= price
+    def sale_goods(self, goods_name, count, price):
+        for goods in self.goods_list:
+            if goods['name'] == goods_name:
+                if goods['count'] == count:
+                    self.goods_list.remove(goods)
+
+                else:
+                    goods['count'] -= count
+            break
+        self.goods_total_count -= count
+        self.one_day_cash += price
+        self.cash += price
+
 
     def go_one_day(self):
+        import random
         self.ndays += 1
         if self.deposit >= 0:
-            self.deposit = self.deposit * conf.INTERESTS
+            deposit = self.deposit + int(self.deposit * conf.INTERESTS)
+            self.deposit = deposit
+        hp_del = random.randrange(1, 4)
+        self.hp -= hp_del
+        input('一天过去了，%s的现银 %s，hp -%s' % (self.name, self.one_day_cash, hp_del))
 
+        self.one_day_cash = 0
+
+
+    def pay(self, money):
+        if self.cash > money:
+            self.cash -= money
+
+    def add_hp(self, hp):
+        if self.hp + hp > 100:
+            self.hp = 100
+        else:
+            self.hp += hp
+
+    def get_hp(self):
+        return self.hp
+
+
+class seller(role):
+    def __init__(self, name):
+        super(seller, self).__init__(name)
+        self.__prices = self.__get_prices()
+
+    def __get_prices(self):
+        import random
+        prices = []
+        for num, goods in enumerate(conf.GOODS_list, 1):
+            price = goods['min'] + random.random() * (goods['max'] - goods['min'] + 1)
+            prices.append(int(price))
+        return prices
+
+    def say_news(self):
+        msg = self.__random_news()
+        input('%s: %s' %(self.name, msg))
+    def get_prices(self):
+        return self.__get_prices()
+
+    def __random_news(self):
+        import random
+        news_list = conf.NEWS_LIST
+        # int rd = rand32() % GOODS_LIST_MAX;
+        #
+        # system( "cls" );
+        #
+        # puts( "-北京新闻播报-\n" );
+        #
+        # //随机选择新闻
+        # int news_id = rlist[rd] * 4 + rand32() % 4;
+        rd = random.randrange(0, len(self.__prices))
+        news_id = rd * 4 + random.randrange(0, 4)
+        # print(len(news_list))
+        #news = news_list[random.randrange(0, 35585 ) % len(news_list)]
+        news = news_list[news_id]
+        # if( news_list[news_id].impact > 0 ){
+        #
+        # 	plist[rd] = ( int )( plist[rd] * news_list[news_id].impact );
+        # }
+        # else if( news_list[news_id].impact < 0 ){
+        #
+        # 	plist[rd] = ( int )( plist[rd] / ( -news_list[news_id].impact ) );
+        # }
+
+        if news['impact'] > 0:
+            # print(prices[news['id']])
+            self.__prices[news['id']] = int(self.__prices[news['id']] * news['impact'])
+            # print(prices[news['id']])
+        else:
+            # print(prices[news['id']])
+            self.__prices[news['id']] = int(self.__prices[news['id']] / (-news['impact']))
+            # print(prices[news['id']])
+        #print('---世界消息---')
+        # input(news['msg'])
+        return news['msg']
 
 
